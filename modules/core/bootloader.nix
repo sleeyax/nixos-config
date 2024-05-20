e444f70727d7e3df5ea8873d4e115da7c0ae9997
Sleeyax <yourd3veloper@gmail.com>
2024-05-20 20:22:59 +0200

    add manjaro boot entry

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -4,4 +4,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.extraEntries."manjaro.conf" = ''
+    title Manjaro
+    efi   /efi/Manjaro/grubx64.efi
+  '';
 }
