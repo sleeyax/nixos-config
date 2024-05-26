321f70a3adb84d78d33b6ae8da3f664561368ee6
Sleeyax <yourd3veloper@gmail.com>
2024-05-26 17:07:26 +0200

    enable GlobalProtect VPN service

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -6,6 +6,7 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+    globalprotect.enable = true;
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
