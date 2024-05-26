f49811e0dc4fd98f5d4d02812feec9fd6537cecf
Sleeyax <yourd3veloper@gmail.com>
2024-05-26 17:54:57 +0200

    allow DNS Multicast UDP port

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -8,7 +8,7 @@
       enable = true;
       logRefusedPackets = true; # journalctl -kr to see logs
       allowedTCPPorts = [ 22 80 443 59010 59011 53317 ];
-      allowedUDPPorts = [ 59010 59011 53317 4501 ];
+      allowedUDPPorts = [ 59010 59011 53317 4501 5353 ];
       # allowedUDPPortRanges = [
         # { from = 4000; to = 4007; }
         # { from = 8000; to = 8010; }
