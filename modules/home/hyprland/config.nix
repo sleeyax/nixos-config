772d6565d6cedc9264fec4cb82696591b8f42e35
Sleeyax <yourd3veloper@gmail.com>
2024-11-28 02:19:58 +0100

    fix hyprland errors

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -54,7 +54,6 @@
       };
 
       dwindle = {
-        no_gaps_when_only = true;
         force_split = 0;
         special_scale_factor = 1.0;
         split_width_multiplier = 1.0;
@@ -66,7 +65,6 @@
       master = {
         new_status = "master";
         special_scale_factor = 1;
-        no_gaps_when_only = false;
       };
 
       decoration = {
@@ -88,14 +86,6 @@
           new_optimizations = true;
           xray = true;
         };
-
-        drop_shadow = true;
-
-        shadow_ignore_window = true;
-        shadow_offset = "0 2";
-        shadow_range = 20;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(00000055)";
       };
 
       animations = {
