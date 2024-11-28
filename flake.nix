1c75ffcc3f0ce44d9e62da4fe617467a338cab5e
Sleeyax <yourd3veloper@gmail.com>
2024-11-28 01:44:15 +0100

    update

--- flake.nix
+++ flake.nix
@@ -27,10 +27,10 @@
       url = "github:catppuccin/bat";
       flake = false;
     };
-    catppuccin-cava = {
-      url = "github:catppuccin/cava";
-      flake = false;
-    };
+    # catppuccin-cava = {
+    #   url = "github:catppuccin/cava";
+    #   flake = false;
+    # };
     catppuccin-starship = {
       url = "github:catppuccin/starship";
       flake = false;
